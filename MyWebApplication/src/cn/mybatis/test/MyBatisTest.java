328d6cf5654b98c4532817c017c614288d2dff7c
iustc <imarklei90@126.com>
2017-07-04 21:50:06 +0800

    commit mybatis select code

---
+++ MyWebApplication/src/cn/mybatis/test/MyBatisTest.java
@@ -0,0 +1,45 @@
+package cn.mybatis.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import cn.mybatis.entity.User;
+
+public class MyBatisTest {
+	
+	// 根据用户ID查询用户信息
+	@Test
+	public void testFindUserById() throws IOException{
+		
+		// 加载MyBatis的配置文件
+		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+		//创建会话工厂,传入MyBatis的配置文件信息
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		//通过工厂得到SqlSession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		// 通过SqlSession操作数据库
+		// 第一个参数：映射文件中Statement的ID，值为：namespace + "." + statement的ID
+		// 第二个参数：指定和映射文件中所匹配的parameterType类型的参数
+		User user = sqlSession.selectOne("test.findUserById", 3);
+		System.out.println(user);
+		
+		//	释放资源
+		sqlSession.close();
+	}
+	
+	// 根据用户名模糊查询
+	@Test
+	public void testFindUserByName() throws IOException{
+		SqlSession sqlSession = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("SqlMapConfig.xml")).openSession();
+		List<User> lists = sqlSession.selectList("test.findUserByName", "xiao"); 
+		System.out.println(lists);
+		sqlSession.close();
+	}
+}
